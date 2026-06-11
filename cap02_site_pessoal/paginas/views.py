e6788259a32a5248227939dc0e703161e1650a33
marivaldocdr <marivaldocdr@gmail.com>
2026-06-11 12:54:52 -0300

    cap02_site_pessoal: templates home.html e sobre.html com URLs configuradas

--- cap02_site_pessoal/paginas/views.py
+++ cap02_site_pessoal/paginas/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Crie suas opiniões aqui.
+def home_page_view(request):
+    return render(request, "paginas/home.html")
+
+
+def about_page_view(request):
+    return render(request, "paginas/sobre.html")
